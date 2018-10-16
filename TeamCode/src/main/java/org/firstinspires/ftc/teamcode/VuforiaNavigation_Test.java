626dd431941f29389537ea124429ffc6b74fc49c
Spencer Huntsman <sphunts01@gmail.com>
2018-10-16 17:28:30 -0600

    Vuforia Work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaNavigation_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaNavigation_Test.java
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.vuforia.HINT;
+import com.vuforia.Vuforia;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
@@ -15,70 +17,102 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefau
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 
 @Autonomous(name = "VuforiaNavigation_Test", group = "Autonomous")
-public class VuforiaNavigation_Test extends LinearOpMode {
+public class VuforiaNavigation_Test extends LinearOpMode
+{
+    // Variables to be used for later
+    private VuforiaLocalizer vuforiaLocalizer;
+    private VuforiaLocalizer.Parameters parameters;
+    private VuforiaTrackables visionTargets;
+    private VuforiaTrackable target;
+    private VuforiaTrackableDefaultListener listener;
 
-    VuforiaLocalizer vuforiaLocalizer;
-    VuforiaLocalizer.Parameters parameters;
-    VuforiaTrackables visionTargets;
-    VuforiaTrackable target;
-    VuforiaTrackableDefaultListener listener;
+    private OpenGLMatrix lastKnownLocation;
+    private OpenGLMatrix phoneLocation;
 
-    OpenGLMatrix lastKnownLocation;
-    OpenGLMatrix phoneLocation;
+    private static final String VUFORIA_KEY = "AdF2fDH/////AAABmQuWoIn1IkJhjVotLJb2ay8WNNW5MqbLJzfKlPU+ThOWdxjTQ2D4h96Yj+liCgL+eSnysijOv9CvpygVAeJysdh8jV3SZtlVrsMWWNWGSHsQ/8xhCofOzUui5NevYRAMwXTsV99CBORiGqMtCQnxKRY4MEFKm6kVVqOnEVtlcS5vErF6BM28eHU3YuX8LOUoXMuH1h3apl8s0eXjTHazCrxpNTy8kXkaIMwYcLcviqfWzsOe6BwFY9Ppf477uJJmM5oNYMqHhHU6TC8vGWE24wJhOpMObrQu3tiavXrvebLjds38AVf/gMF25WY+H6dgKh56Y/aV6DqmLo24qwLRF67C4j9G1S5rB05v8TajW30A"; // Insert your own key here
 
-    public static final String VUFORIA_KEY = "AdF2fDH/////AAABmQuWoIn1IkJhjVotLJb2ay8WNNW5MqbLJzfKlPU+ThOWdxjTQ2D4h96Yj+liCgL+eSnysijOv9CvpygVAeJysdh8jV3SZtlVrsMWWNWGSHsQ/8xhCofOzUui5NevYRAMwXTsV99CBORiGqMtCQnxKRY4MEFKm6kVVqOnEVtlcS5vErF6BM28eHU3YuX8LOUoXMuH1h3apl8s0eXjTHazCrxpNTy8kXkaIMwYcLcviqfWzsOe6BwFY9Ppf477uJJmM5oNYMqHhHU6TC8vGWE24wJhOpMObrQu3tiavXrvebLjds38AVf/gMF25WY+H6dgKh56Y/aV6DqmLo24qwLRF67C4j9G1S5rB05v8TajW30A";
+    private float robotX = 0;
+    private float robotY = 0;
+    private float robotAngle = 0;
 
-    public void runOpMode() {
-
+    public void runOpMode() throws InterruptedException
+    {
         setupVuforia();
 
+        // We don't know where the robot is, so set it to the origin
+        // If we don't include this, it would be null, which would cause errors later on
         lastKnownLocation = createMatrix(0, 0, 0, 0, 0, 0);
 
         waitForStart();
 
+        // Start tracking the targets
         visionTargets.activate();
 
-        while (opModeIsActive()) {
-
+        while(opModeIsActive())
+        {
+            // Ask the listener for the latest information on where the robot is
             OpenGLMatrix latestLocation = listener.getUpdatedRobotLocation();
 
-            if (latestLocation != null) {
+            // The listener will sometimes return null, so we check for that to prevent errors
+            if(latestLocation != null) {
                 lastKnownLocation = latestLocation;
             }
 
-            telemetry.addData("Tracking: " + target.getName(), listener.isVisible());
-            telemetry.addData("Last Known Location: " + formatMatrix(lastKnownLocation), listener.isVisible());
+            float[] coordinates = lastKnownLocation.getTranslation().getData();
 
+            robotX = coordinates[0];
+            robotY = coordinates[1];
+            robotAngle = Orientation.getOrientation(lastKnownLocation, AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).thirdAngle;
+
+            // Send information about whether the target is visible, and where the robot is
+            telemetry.addData("Tracking " + target.getName(), listener.isVisible());
+            telemetry.addData("Last Known Location", "X:"+robotX + "Y:"+robotY);
+
+            // Send telemetry and idle to let hardware catch up
             telemetry.update();
             idle();
-
         }
-
     }
 
-    public void setupVuforia() {
-        parameters = new VuforiaLocalizer.Parameters(R.id.cameraMonitorViewId);
+    private void setupVuforia()
+    {
+        // Setup parameters to create localizer
+        parameters = new VuforiaLocalizer.Parameters(R.id.cameraMonitorViewId); // To remove the camera view from the screen, remove the R.id.cameraMonitorViewId
         parameters.vuforiaLicenseKey = VUFORIA_KEY;
         parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        parameters.useExtendedTracking = false;
         vuforiaLocalizer = ClassFactory.createVuforiaLocalizer(parameters);
 
+        // These are the vision targets that we want to use
+        // The string needs to be the name of the appropriate .xml file in the assets folder
         visionTargets = vuforiaLocalizer.loadTrackablesFromAsset("2018-PicturesDB");
+        Vuforia.setHint(HINT.HINT_MAX_SIMULTANEOUS_IMAGE_TARGETS, 2);
 
-        target = visionTargets.get(0);
-        target.setName("Nebula");
+        // Setup the target to be tracked
+        target = visionTargets.get(0); // 0 corresponds to the moon target
+        target.setName("Moon Target");
         target.setLocation(createMatrix(0, 500, 0, 90, 0, 90));
 
-        phoneLocation = createMatrix(0, 0, 0, 0, 0,0);
+        // Set phone location on robot
+        phoneLocation = createMatrix(0, 225, 0, 90, 0, 0);
 
+        // Setup listener and inform it of phone information
         listener = (VuforiaTrackableDefaultListener) target.getListener();
         listener.setPhoneInformation(phoneLocation, parameters.cameraDirection);
     }
 
-    public OpenGLMatrix createMatrix(float x, float y, float z, float u, float v, float w) {
-        return OpenGLMatrix.translation(x,y,x).multiplied(Orientation.getRotationMatrix(AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES, u, v, w));
+    // Creates a matrix for determining the locations and orientations of objects
+    // Units are millimeters for x, y, and z, and degrees for u, v, and w
+    private OpenGLMatrix createMatrix(float x, float y, float z, float u, float v, float w)
+    {
+        return OpenGLMatrix.translation(x, y, z).
+                multiplied(Orientation.getRotationMatrix(
+                        AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES, u, v, w));
     }
 
-    public String formatMatrix(OpenGLMatrix matrix) {
+    // Formats a matrix into a readable string
+    private String formatMatrix(OpenGLMatrix matrix)
+    {
         return matrix.formatAsTransform();
     }
 }
